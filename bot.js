cc3cd943cce8a7653cdae796c93a89d71043e61a
KIMMOJI <59527466+kimmojiz@users.noreply.github.com>
2021-08-16 18:45:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,87 @@
-const discord = require("discord.js")
-const client = new discord.Client()
-const prefix = "!"
-const { token } = require("./data.json")
-const axios = require("axios")
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+//require npm [ discord.js, axios ]
 
-client.on('message',async (message) => {
-  const args = message.content.trim().split(/ +/g);
-  const cmd = args[0].slice(prefix.length).toLowerCase();
-  if (cmd === 'user') {
-    switch(args[1]) {
-      case "cookie":
-    axios({
-      method: 'GET',
-      url: 'https://www.roblox.com/mobileapi/userinfo',
-      headers: { "Cookie": '.ROBLOSECURITY='+ args[2] }
-    
-    })
-    .then(
-      function(response) {
-        if (response.data.UserID === undefined) {
-          var ebs = new discord.MessageEmbed()
-          .setColor("RED")
-          .setAuthor("Error!", "https://cdn.icon-icons.com/icons2/1380/PNG/512/vcsconflicting_93497.png")
-          .setDescription("Cookie ผิดพลาด")
-          .setThumbnail("https://cdn.pixabay.com/photo/2016/03/31/18/31/dialog-1294429_960_720.png")
-          .setTimestamp();
-        message.channel.send(ebs)
-        return;
+const { Client, MessageEmbed } = require('discord.js');
+const axios = require('axios');
+
+const client = new Client({ intents: 32767 });
+
+const config = {
+    //token bot!
+    token: 'token here',
+    //bot prefix!
+    prefix: '!',
+    //main color
+    color: '#03b9f5'
+}
+
+client.on('ready', () => { console.log('Code by KIMMOJI.') });
+
+client.on(
+    "messageCreate",
+        async (message) => {
+            const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+            const cmd = args.shift().toLowerCase();
+
+
+            switch (cmd) {
+                case 'login':
+                    try {
+                    if (message.guild.me.hasPermission('MANAGE_MESSSAGE')) {
+                        throw '`not have manager message permission.`'
+                    } else { message.delete() }
+                    
+                    if (!args[0]) throw '`args is empty, Please try again.`'
+
+                    await axios.get('https://www.roblox.com/mobileapi/userinfo', {
+                        headers: { "Cookie": '.ROBLOSECURITY='+ args[0]}
+                    })
+                    .then(
+                        function(response) {
+                            if (response.data.UserID === undefined) throw 'Fail cookie.'
+
+                            var user = response.data;
+
+                            message.channel.send({
+                                embeds: [
+                                new MessageEmbed()
+                                    .setColor('GREEN')
+                                    .setAuthor(
+                                        client.user.tag,
+                                        client.user.displayAvatarURL(
+                                            { dynamic: true }
+                                        )
+                                    )
+                                    .addFields(
+                                        {
+                                            name: 'Username',
+                                            value: user.UserName,
+                                            inline: false
+                                        }, {
+                                            name: 'UserID',
+                                            value: (user.UserID).toString(),
+                                            inline: false 
+                                        }, {
+                                            name: 'Robux',
+                                            value: (user.RobuxBalance).toString(),
+                                            inline: false 
+                                        }
+                                    )
+                                    .setThumbnail(user.ThumbnailUrl)
+                                    .setTimestamp()
+                                    .setFooter('Code by KIMMOJI')
+                                ]
+                            })
+                            
+                        }
+                    )
+                    .catch( () => { throw '`Fail cookie, Please try again.`' })
+
+                    } catch(err) {
+                        return message.reply('**[!] :** ' + err)
+                    }
+                break;
+            }
         }
-        var eb = new discord.MessageEmbed()
-          .setColor("GREEN")
-          .setAuthor("Profile Roblox", "https://www.pngfind.com/pngs/m/80-804949_profile-icon-for-the-politics-category-circle-hd.png")
-          .addField("ชื่อผู้ใช้", response.data.UserName)
-          .addField("ID ผู้ใช้", response.data.UserID)
-          .addField("Robux", response.data.RobuxBalance)
-          .addField("Premium", response.data.IsPremium)
-          .setThumbnail(response.data.ThumbnailUrl)
-          .setTimestamp()
-        message.channel.send(eb)
-      })
-      .catch(function(error) {
-        var eb = new discord.MessageEmbed()
-          .setColor("RED")
-          .setAuthor("Error!", "https://cdn.icon-icons.com/icons2/1380/PNG/512/vcsconflicting_93497.png")
-          .setDescription("เกิดข้อผิดผลาดกับ API")
-          .setThumbnail("https://cdn.pixabay.com/photo/2016/03/31/18/31/dialog-1294429_960_720.png")
-          .setTimestamp();
-        message.channel.send(eb)
-        console.log(error)
-      });
-      break;
-    }
-  }
-});
+);
 
-client.login(token).catch((error) => {console.log(error)});
+client.login(config.token).catch((e) => { console.log(e) });
